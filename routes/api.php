85e5c9711c1a4682cd3ac67c1a55b75e475e3cae
sakib3 <sakib3@gmail.com>
2018-09-15 14:40:22 +0200

    Products api

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,10 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::get('products', ['as' => 'products', function () {
+    return App\ Product::all();
+}]);
--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -5,16 +5,16 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class ExampleTest extends TestCase
+class ProductsTest extends TestCase
 {
     /**
      * A basic test example.
      *
      * @return void
      */
-    public function testBasicTest()
+    public function testProductsApiReturn200()
     {
-        $response = $this->get('/');
+        $response = $this->get('/api/products');
 
         $response->assertStatus(200);
     }
